d71427c8fcf9e1a25f7e1a398ce3f0394e6ff7e8
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-06 01:22:42 +0300

    Added changes on loadmarker from json

--- applet-1.js
+++ applet-1.js
@@ -107,9 +107,9 @@ updateLogDisplay() {
 }
 const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
 
-Mymap.loadMarkersFromJson('applet-2.json');
+Mymap.loadMarkersFromJson('applet-1.json');
 
 document.addEventListener('DOMContentLoaded', () => {
     Mymap.displayLogCount();
-    Mymap.loadMarkersFromJson('applet-2.json');
+    Mymap.loadMarkersFromJson('applet-1.json');
 });
