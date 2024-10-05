f0b37392c9f5ab6185fa166ebb3857b469a1e8b5
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-06 00:51:08 +0300

    Added initTileLayer method and syntax from leafletmap

--- applet-1.js
+++ applet-1.js
@@ -2,5 +2,12 @@ class LeafletMap{
 
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
+        this.initTileLayer();
+
+}
+initTileLayer() {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Sample for new corales BSIT student'
+    }).addTo(this.map);
 }
 }
