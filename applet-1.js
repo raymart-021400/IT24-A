b2c910f3f40abae01f28805ff0750278945ce144
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-06 01:19:53 +0300

    Added MyMap load markers

--- applet-1.js
+++ applet-1.js
@@ -106,3 +106,10 @@ updateLogDisplay() {
 }
 }
 const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
+
+Mymap.loadMarkersFromJson('applet-2.json');
+
+document.addEventListener('DOMContentLoaded', () => {
+    Mymap.displayLogCount();
+    Mymap.loadMarkersFromJson('applet-2.json');
+});
