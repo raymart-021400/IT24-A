a0ad54fa6e326571d3d0f7c13d589761ab136753
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-07 00:18:57 +0300

    Added count logs

--- applet-2.js
+++ applet-2.js
@@ -35,4 +35,11 @@ class DataLogger{
         
         this.displayLogCount();
     }
+    countLogs(){
+        return this.loggedData.length;
+      }
+      displayLogCount() {
+          const logCount = this.countLogs();
+          this.logCountElement.innerHTML = `<p>Total Logs: ${logCount}</p>`;
+      }
 }
