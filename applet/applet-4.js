6f2e456a7581edc9533a02593d8b27ebc1d5a6b6
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-09 21:53:29 +0300

    added init() and fetchdata()

--- applet/applet-4.js
+++ applet/applet-4.js
@@ -5,4 +5,18 @@ class StudentList {
         this.students = [];
         this.init();
     }
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+   
+    }
+
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
 }
