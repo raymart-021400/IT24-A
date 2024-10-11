91c7af89dec4c19d76e455b0266b2f5e41e53a3c
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-11 09:50:30 +0300

    Added filterCards method

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ init() {
     this.searchInput.addEventListener('input', () => {
         this.filterCards(this.searchInput.value);
     });
-}}
+}
+filterCards(query) {
+    const searchTerm = query.toLowerCase();
+
+    this.cards.forEach(card => {
+        const title = card.querySelector('.card-title').textContent.toLowerCase();
+        const text = card.querySelector('.card-text').textContent.toLowerCase();
+
+        if (title.includes(searchTerm) || text.includes(searchTerm)) {
+            card.closest('.col-sm-4').style.display = '';
+        } else {
+            card.closest('.col-sm-4').style.display = 'none'; 
+        }
+    });
+}
+}
