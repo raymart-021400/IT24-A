4e82482ad4f5276c7f276584af769c000800bf4a
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-11 09:48:52 +0300

    Added init method

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ class cardSearch{
         this.searchInput = document.getElementById(searchInputId);
         this.cardContainer = document.querySelector(`.${cardContainerClass}`);
         this.cards = this.cardContainer.querySelectorAll('.card');
-    }
+        this.init();
 }
+
+init() {
+    this.searchInput.addEventListener('input', () => {
+        this.filterCards(this.searchInput.value);
+    });
+}}
