b256716ac22e230b4f7d18c476a65e6e3ab37f06
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-17 11:06:01 +0800

    added instance and evenListener

--- applet-5.js
+++ applet-5.js
@@ -1,5 +1,19 @@
 class WeatherApp {
     constructor(apiKey) {
         this.apiKey = apiKey;
+
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+        this.getLocationBtn = document.getElementById('getLocationBtn');
+        this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+        this.apiKeyInput = document.getElementById('apiInput');
+
+        this.getWeatherBtn.addEventListener('click', () => this.fetchWeather());
+        this.getLocationBtn.addEventListener('click', () => this.fetchWeatherByLocation());
     }
 }
