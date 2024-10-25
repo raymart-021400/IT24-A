f1ee73072675e2ef943992eb4a779a091375737a
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-25 11:17:31 +0800

    added addOrUpdatedTask method

--- applet-6.js
+++ applet-6.js
@@ -6,9 +6,32 @@ class todoList{
         this.todoList = document.getElementById('todoList');
 
         this.addButton.addEventListener('click', () => this.addOrUpdateTask());
-        this.todoList.addEventListener('click'), (e) => {
+        this.todoList.addEventListener('click', (e) => {
             const action = e.target.classList.contains('removeButton') ? 'remove' : e.target.classList.contains('editButton') ? 'edit' : e.taget.classList.contains('doneButton') ? 'done' : null;
             if (action) this [action + 'Task'](e);
                 });
             }
-        }
+            addOrUpdateTask(){
+                const taskTest = this.todoInput.value.trim();
+                if (taskTest){
+                    this.editingIndex === -1 ? this.addTask(taskText) : this.updateTask(taskText);
+                    this.todoInput.value = '';
+                }
+            }
+            addTask(taskText){
+                const listItem = document.createElement('li');
+                listItem.className = 'list-group-item todo-item';
+                listItem.innerHTML = `
+                    <div class="card">
+                    <span class="task-text">${taskText}</span>
+                    <span class="timestamp" style="display: block; margin-top: 0.5rem; color: gray;">Date Added: ${new Date().toLocaleString()}</span>
+                    <div style="margin-top: 0.5rem;">
+                        <button class="btn btn-success btn-sm doneButton">Done</button>
+                        <button class="btn btn-warning btn-sm editButton">Edit</button>
+                        <button class="btn btn-danger btn-sm removeButton">Remove</button>
+                    </div>
+                     </div>
+                `;
+                this.todoList.appendChild(listItem);
+            }
+}
