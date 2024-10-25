b8283f24f1c493f5e70f7b0e9cc6075e47df0f65
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-25 10:50:55 +0800

    added instance and event listener in constructor

--- applet-6.js
+++ applet-6.js
@@ -1,5 +1,14 @@
 class todoList{
     constructor(){
-        
-    }
-}
+        this.editingIndex = -1;
+        this.addButton = document.getElemenyById('addButton');
+        this.todoInput = document.getElementById('todoInput');
+        this.todoList = document.getElementById('todoList');
+
+        this.addButton.addEventListener('click', () => this.addOrUpdateTask());
+        this.todoList.addEventListener('click'), (e) => {
+            const action = e.target.classList.contains('removeButton') ? 'remove' : e.target.classList.contains('editButton') ? 'edit' : e.taget.classList.contains('doneButton') ? 'done' : null;
+            if (action) this [action + 'Task'](e);
+                });
+            }
+        }
